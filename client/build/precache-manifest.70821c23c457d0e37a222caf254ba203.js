7d156731aba29615b2fae664a3c514202b1c834c
YASH-KHANKHOJE <yash.khankhoje@gmail.com>
2021-07-26 14:53:11 +0530

    first commit

---
+++ client/build/precache-manifest.70821c23c457d0e37a222caf254ba203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ae7db310825dd08e1522484caabd1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31ea1bd3cf4a5f1758cb",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ea1bd3cf4a5f1758cb",
+    "url": "/static/js/main.28b28b1b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
